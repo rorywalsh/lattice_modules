8e28efb79fac46c03765caf740816daba5dcac38
rorywalsh <rwalsh@Rorys-Mac-mini.fritz.box>
2021-11-29 09:18:24 +0000

    updating interface

--- src/LatticeExternalProcessor.h
+++ src/LatticeExternalProcessor.h
@@ -6,11 +6,13 @@
 #include <map>
 
 
+
 template <typename... Types>
 void ignoreParameters (Types&&...) noexcept {}
 
 /* parameter structure - takes parameter name and
  a vector holding [min, max, increment, defaultValue, skew]*/
+//String name, float min, float max, float value, float interval, float skew = 1
 struct ExternalParameter
 {
     std::vector<float> range;
@@ -37,38 +39,28 @@ public:
     /* Main processing function - paramValues is a list of parameter values passed from the host in
      order of their creation */
     virtual void process(float** buffer, int numChannels, int blockSize, std::vector<std::atomic<float>*> paramValues) = 0;
-    
+    /* Called by the host when a paremeter changes */
+    virtual void hostParameterChanged(const std::string& parameterID, float newValue) = 0;
     /* Used to query the current value of a parameter - the names correspond to the names used
         in the createParameters() function */
-    float getParameter(std::string name)
-    {
-        return paramMap.at(name)->load();
-    }
-    
+    float getParameter(std::string name)    {        return paramMap.at(name)->load();    }
     /* called by the host when a note is started */
-    void startNote(int midiNoteNumber, float velocity)
-    {
-        
-    }
-    
+    virtual void startNote(int midiNoteNumber, float velocity)    {            }
     /* called by the host when a note is stoped */
-    void stopNote (float velocity)
-    {
-        
-    }
-    
-    double getMidiNoteInHertz (const int noteNumber, const double Atuning)
-    {
-        return Atuning * std::pow (2.0, (noteNumber - 69) / 12.0);
-    }
+    virtual void stopNote (float velocity)    {            }
+    /* Used to get frequency from Midi note */
+    double getMidiNoteInHertz (const int noteNumber, const double Atuning)    {        return Atuning * std::pow (2.0, (noteNumber - 69) / 12.0);    }
     
     /* Called by the host to fill paramValues*/
-    virtual void mapParameters(std::vector<std::atomic<float>*> paramValues)
+    virtual void mapParameters(std::vector<std::atomic<float>*> paramValues, std::vector<std::string> paramNames)
     {
-        paramMap["Gain"] = paramValues[0];
+        for(int i = 0 ; i < paramNames.size() ; i++)
+            paramMap[paramNames[i]] = paramValues[0];
     }
     
+    /* Called by host when a Midi note is triggered */
     void setMidiNoteNumber(int noteNumber)  { midiNoteNumber = noteNumber;      }
+    /* Used to query current Midi note */
     int getMidiNoteNumber()                 { return midiNoteNumber;            }
 
     
