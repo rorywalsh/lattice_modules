c4f48e345fd8c87f1bf54bb1b72f53c4be4f504c
Victor Lazzarini <victor.lazzarini@mu.ie>
2022-03-26 15:35:08 +0000

    added freverb example

---
+++ examples/freverb/FreverbProcessor.cpp
@@ -0,0 +1,81 @@
+
+#include "FreverbProcessor.h"
+#include <iterator>
+
+
+FreverbProcessor::FreverbProcessor()
+  : reverbL(1.f,10000), reverbR(1.f,10000), inL(Aurora::def_vsize), inR(Aurora::def_vsize), fs(def_sr)
+{
+    
+}
+
+LatticeProcessorModule::ParameterData FreverbProcessor::createParameters()
+{
+    addParameter({ "Reverb Time", {0, 5, 1.f, .01f, 1}});
+    addParameter({ "Lowpass Frequency", {100, 20000, 10000.f, 1.f, 1}});
+    addParameter({ "Reverb Mix", {0, 1.f, .5f, 0.001f, 1}});
+    return ParameterData(getParameters(), getNumberOfParameters());
+}
+
+
+void FreverbProcessor::prepareProcessor(int sr, std::size_t block)
+{
+   reverbL.reset(1.f, 10000.f,sr);
+   reverbR.reset(1.f,10000.f,sr);
+   fs = sr;
+}
+
+
+void FreverbProcessor::triggerParameterUpdate(const std::string& parameterID, float newValue)
+{
+    updateParameter(parameterID, newValue);
+}
+
+LatticeProcessorModule::ChannelData FreverbProcessor::createChannels()
+{
+    addChannel({"Input 1", ChannelType::input });
+    addChannel({"Input 2", ChannelType::input });
+    addChannel({"Output 1", ChannelType::output });
+    addChannel({"Output 2", ChannelType::output });
+    return ChannelData(getChannels(), getNumberOfChannels());
+}
+
+void FreverbProcessor::process(float** buffer, int numChannels, std::size_t blockSize, const HostData)
+{
+   
+    inL.resize(blockSize);
+    inR.resize(blockSize);
+    std::copy(buffer[0], buffer[0] + blockSize, inL.begin());
+    std::copy(buffer[1], buffer[1] + blockSize, inR.begin());
+    reverbL.reverb_time(getParameter("Reverb Time"));
+    reverbR.reverb_time(getParameter("Reverb Time"));
+    reverbL.lp_freq(getParameter("Lowpass Frequency"),fs);
+    reverbR.lp_freq(getParameter("Lowpass Frequency"),fs);
+
+    auto &l = reverbL(inL, getParameter("Reverb Mix"));
+    auto &r = reverbR(inR, getParameter("Reverb Mix"));
+
+    for(int i = 0; i < blockSize ; i++)
+    {
+        buffer[0][i] = l[i] * 0.1 - r[i] * 0.13;
+        buffer[1][i] = r[i] * 0.1 - l[i] * 0.13;
+    }
+    
+}
+
+
+// the class factories
+#ifdef WIN32
+extern "C" 
+{
+	__declspec(dllexport) LatticeProcessorModule* create() { return new FreverbProcessor; }
+};
+
+extern "C" 
+{
+	__declspec(dllexport) void destroy(LatticeProcessorModule* p) { delete p; }
+};
+#else
+extern "C" LatticeProcessorModule* create(){             return new FreverbProcessor;         }
+extern "C" void destroy(LatticeProcessorModule* p){      delete p;                     }
+#endif
---
+++ examples/freverb/FreverbProcessor.h
@@ -0,0 +1,107 @@
+#pragma once
+#include "LatticeProcessorModule.h"
+#include "Del.h"
+#include "Osc.h"
+#include <array>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using namespace Aurora;
+template <typename S> inline S scl(S a, S b) { return a * b; }
+  
+class FreverbProcessor : public LatticeProcessorModule
+{
+  
+  template <typename S> struct Reverb {
+  static constexpr S dt[4] = {0.037, 0.031, 0.029, 0.023};
+  static constexpr S adt[2] = {0.01, 0.0017};
+
+  std::array<Del<S, lp_delay>, 4> combs;
+  std::array<Del<S>, 2> apfs;
+  Mix<S> mix;
+  BinOp<S, scl> gain;
+  std::array<std::vector<S>, 4> mem;
+  std::array<S, 4> g;
+  S rvt;
+
+  void reverb_time(S rvt) {
+    std::size_t n = 0;
+    for (auto &gs : g)
+      gs = std::pow(.001, dt[n++] / rvt);
+  }
+
+  void lp_freq(S lpf, S fs) {
+    for (auto &m : mem) {
+      m[0] = 0;
+      double c = 2. - std::cos(2 * M_PI * lpf / fs);
+      m[1] = sqrt(c * c - 1.f) - c;
+    }
+  }
+
+  void reset(S rvt, S lpf, S fs) {
+    std::size_t n = 0;
+    for (auto &obj : combs)
+      obj.reset(dt[n++], fs);
+    apfs[0].reset(adt[0], fs);
+    apfs[1].reset(adt[1], fs);
+    reverb_time(rvt);
+    lp_freq(lpf, fs);
+  }
+
+  Reverb(S rvt, S lpf, S fs = def_sr, std::size_t vsize = def_vsize)
+      : combs({Del<S, lp_delay>(dt[0], fs, vsize),
+               Del<S, lp_delay>(dt[1], fs, vsize),
+               Del<S, lp_delay>(dt[2], fs, vsize),
+               Del<S, lp_delay>(dt[3], fs, vsize)}),
+        apfs({Del<S>(adt[0], fs, vsize), Del<S>(adt[1], fs, vsize)}),
+        mix(vsize), gain(vsize), mem({std::vector<S>(2), std::vector<S>(2),
+                                      std::vector<S>(2), std::vector<S>(2)}),
+        g({0, 0, 0, 0}) {
+    reverb_time(rvt);
+    lp_freq(lpf, fs);
+  };
+
+  const std::vector<S> &operator()(const std::vector<S> &in, S rmx) {
+    S ga0 = 0.7;
+    S ga1 = 0.7;
+    auto &s = gain(0.25, mix(combs[0](in, 0, g[0], 0, &mem[0]),
+                             combs[1](in, 0, g[1], 0, &mem[1]),
+                             combs[2](in, 0, g[2], 0, &mem[2]),
+                             combs[3](in, 0, g[3], 0, &mem[3])));
+    return mix(in, gain(rmx, apfs[1](apfs[0](s, 0, ga0, -ga0), 0, ga1, -ga1)));
+  }
+};
+
+public:
+    FreverbProcessor();
+    
+    
+    /* This function is called by he host to populate the parameter vector */
+    ChannelData createChannels() override;
+    
+    ParameterData createParameters() override;
+    
+    /*  This function is called by the host before playback/performance */
+    void prepareProcessor(int sr, std::size_t block) override;
+    
+    /* Call this method to trigger host callback */
+    void triggerParameterUpdate(const std::string& parameterID, float newValue);
+    
+    /*  Main processing function called continuously by the host on the audio thread.
+        paramValues is a list of parameter values passed from the host in order of their creation */
+    void process(float** buffer, int numChannels, std::size_t blockSize, const HostData hostInfo) override;
+    
+    const char* getModuleName() override
+    {
+        return "Stereo Reverb";
+    }
+private:
+    Reverb<float> reverbL;
+    Reverb<float> reverbR;
+    std::vector<float> inL;
+    std::vector<float> inR;
+    float fs;
+};
+
