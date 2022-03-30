d0f9b1520df2e90cbedbf3fb88ccc6f90992fa23
Victor Lazzarini <victor.lazzarini@mu.ie>
2022-03-30 13:58:33 +0100

    added tvconv ... fixed filter for now because I can't update parameters

---
+++ examples/tvconv/TVConvProcessor.cpp
@@ -0,0 +1,89 @@
+
+#include "TVConvProcessor.h"
+#include <iterator>
+#include <sstream>
+
+
+TVConvProcessor::TVConvProcessor()
+  : conv(16384), in1(Aurora::def_vsize), in2(Aurora::def_vsize), fs(Aurora::def_sr), len(16384)
+{
+	
+}
+
+LatticeProcessorModule::ChannelData TVConvProcessor::createChannels()
+{
+    addChannel({ "Input 1", LatticeProcessorModule::ChannelType::input });
+    addChannel({ "Input 2", LatticeProcessorModule::ChannelType::input });
+    addChannel({ "Output 1", LatticeProcessorModule::ChannelType::output });
+    addChannel({ "Output 2", LatticeProcessorModule::ChannelType::output });
+    return ChannelData(getChannels(), getNumberOfChannels());
+}
+
+
+LatticeProcessorModule::ParameterData TVConvProcessor::createParameters()
+{
+    addParameter({ "Gain", {0, 1.f, .5f, .01f, 1}});
+    addParameter({ "Filter Length", {0, 1.f, .1f, .01f, 1}});
+    addParameter({ "Bypass", {0, 1, 0, 1, 1}, "", ModuleParameter::ParamType::Switch});
+    return ParameterData(getParameters(), getNumberOfParameters());
+}
+
+
+void TVConvProcessor::hostParameterChanged(const char* parameterID, const char* newValue)
+{
+  //const std::string paramName = getParameterNameFromId(parameterID);
+    //if(paramName == "Filter Length")
+    //{
+    //std::size_t filter_length =  getParameter("Filter Length")*fs;
+      //conv.reset(filter_length > def_psize ? filter_length : def_psize);
+     std::cout << "Filter Length: " << std::endl;
+      //}
+}
+
+
+
+void TVConvProcessor::prepareProcessor(int sr, std::size_t block)
+{
+  fs  = sr;
+  std::size_t filter_length =  getParameter("Filter Length")*fs;
+  conv.reset(filter_length > def_psize ? filter_length : def_psize);
+  std::cout << "Filter Length: " << filter_length << std::endl;
+  
+}
+
+
+void TVConvProcessor::process(float** buffer, int /*numChannels*/, std::size_t blockSize, const HostData)
+{
+    if (getParameter("Bypass")==0)
+    {
+      in1.resize(blockSize);
+      in2.resize(blockSize);
+      std::copy(buffer[0], buffer[0]+blockSize,in1.begin());
+      std::copy(buffer[1], buffer[1]+blockSize,in2.begin());
+      auto& out = conv(in1,in2, getParameter("Gain"));
+      std::copy(out.cbegin(),out.cend(),buffer[0]);
+      std::copy(out.cbegin(),out.cend(),buffer[1]);
+        
+    }
+    else
+    {
+      std::fill(buffer[0], buffer[0]+blockSize,0);
+      std::fill(buffer[1], buffer[1]+blockSize,0);
+    }
+}
+
+// the class factories
+#ifdef WIN32
+extern "C" 
+{
+	__declspec(dllexport) LatticeProcessorModule* create() { return new TVConvProcessor; }
+};
+
+extern "C" 
+{
+	__declspec(dllexport) void destroy(LatticeProcessorModule* p) { delete p; }
+};
+#else
+extern "C" LatticeProcessorModule* create(){             return new TVConvProcessor;         }
+extern "C" void destroy(LatticeProcessorModule* p){      delete p;                     }
+#endif
---
+++ examples/tvconv/TVConvProcessor.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "LatticeProcessorModule.h"
+#include <iterator>
+#include "Conv.h"
+#include "simple_svg_1.0.0.hpp"
+
+using namespace Aurora;
+
+class TVConvProcessor : public LatticeProcessorModule
+{
+
+
+
+  
+public:
+    TVConvProcessor();
+    
+    ChannelData createChannels() override;
+    
+    /* This function is called by he host to populate the parameter vector */
+    ParameterData createParameters() override;
+    
+    void hostParameterChanged(const char* parameterID, const char* newValue) override;
+    /*  This function is called by the host before playback/performance */
+    void prepareProcessor(int sr, std::size_t block) override;
+    
+    /*  Main processing function called continuously by the host on the audio thread.
+        paramValues is a list of parameter values passed from the host in order of their creation */
+    void process(float** buffer, int numChannels, std::size_t blockSize, const HostData hostInfo) override;
+    
+    void createDescription(std::string& description)
+    {
+        description = "";
+    }
+ 
+
+    static float remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+    
+    const char* getModuleName() override {    return "TV Conv";     }
+    
+private:
+    TVConv<float> conv;
+    bool isPlaying = false;
+    int sampleIndex = 0;
+    std::vector<float> in1, in2;
+    float fs;
+    int len;
+    bool bypass = false;
+};
+
