1c12ad8f57aad138a7ed00f3423602212a50dcdd
rorywalsh <rorywalsh@ear.ie>
2022-04-17 15:49:16 +0100

    updating soundfiler player to accept MIDI input - Version number:

--- examples/file_player/FilePlayerProcessor.h
+++ examples/file_player/FilePlayerProcessor.h
@@ -44,6 +44,11 @@ public:
         return draw;
     }
 
+    bool acceptsMidiInput() override
+    {
+        return true;
+    }
+
     static float remap(float value, float from1, float to1, float from2, float to2)
     {
         return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
