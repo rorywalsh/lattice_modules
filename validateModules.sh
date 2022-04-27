fc54e7c45b9eee38e6298dc4c28b92f62cb225f3
rorywalsh <rwalsh@Rorys-Mac-mini.fritz.box>
2022-04-27 10:56:13 +0100

    updating

--- validateModules.sh
+++ validateModules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for lib in *; do 
-if [ $lib == *.dylib ]; then 
+if [[ $lib == *.dylib ]]; then 
 echo "Signing $lib and removing from com.apple.quarantine"; 
 sudo codesign -s - $lib --timestamp --deep --force
 xattr -d com.apple.quarantine $lib
