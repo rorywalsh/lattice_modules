ff4c9818ce1f78d205e40e8c317db43cec2f9e2e
rorywalsh <rwalsh@Rorys-Mac-mini.fritz.box>
2022-04-28 11:07:56 +0100

    updating validate script so it can be run from anywhere

--- validateModules.sh
+++ validateModules.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for lib in *; do 
+libDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+for lib in $libDir/*; do 
 if [[ $lib == *.dylib ]]; then 
 echo "Signing $lib and removing from com.apple.quarantine"; 
 sudo codesign -s - $lib --timestamp --deep --force
