d2fc402b970f72ed9d3d20582f3aac95c007e28d
rorywalsh <rwalsh@Rorys-Mac-mini.fritz.box>
2022-05-12 16:47:00 +0100

    adding random example - unfinished pof

---
+++ examples/random/RandomProcessor.cpp
@@ -0,0 +1,75 @@
+
+#include "RandomProcessor.h"
+#include <iterator>
+#include <sstream>
+
+
+RandomProcessor::RandomProcessor()
+{
+}
+
+LatticeProcessorModule::ChannelData RandomProcessor::createChannels()
+{
+    addChannel({ "Output", ChannelType::output });
+    return ChannelData(getChannels(), getNumberOfChannels());
+}
+
+
+LatticeProcessorModule::ParameterData RandomProcessor::createParameters()
+{
+    addParameter({ "Frequency", {0, 1000, 1.f, 0.001f, 0.5f}});
+    addParameter(LatticeProcessorModule::Parameter("Strength", {0, 100, 1.0f, 0.001f, 1.f}).withHint("%"));
+    addParameter({ "Increment", {0, 1, 0.01f, 0.001f, 1.f}});
+    return ParameterData(getParameters(), getNumberOfParameters());
+}
+
+
+void RandomProcessor::prepareProcessor(int sr, std::size_t block)
+{
+    samplingRate = sr;
+}
+
+void RandomProcessor::hostParameterChanged(const char* parameterID, float newValue)
+{
+    const std::string paramName = getParameterNameFromId(parameterID);
+    updateParameter(paramName, newValue);
+    
+    if(paramName == "Frequency")
+        sampleIndex = 0;
+    
+}
+
+void RandomProcessor::process(float** buffer, int /*numChannels*/, std::size_t blockSize, const HostData /*hostInfo*/)
+{
+    for( int i = 0 ; i < blockSize ; i++)
+    {
+        if(static_cast<int>((1 / getParameter("Frequency")) * samplingRate) == sampleIndex)
+        {
+            auto increment = (getParameter("Increment") > 0 ? getParameter("Increment") : 0.001f);
+            auto randVal = static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(1.f)));
+            returnValue = (round(randVal / increment) * increment) * (getParameter("Strength") / 100.f);
+            sampleIndex = 0;
+        }
+        
+        buffer[0][i] = returnValue;
+        sampleIndex++;
+    }
+    
+}
+
+
+// the class factories
+#ifdef WIN32
+extern "C"
+{
+    __declspec(dllexport) LatticeProcessorModule* create() { return new RandomProcessor; }
+};
+
+extern "C"
+{
+    __declspec(dllexport) void destroy(LatticeProcessorModule* p) { delete p; }
+};
+#else
+extern "C" LatticeProcessorModule* create(){             return new RandomProcessor;         }
+extern "C" void destroy(LatticeProcessorModule* p){      delete p;                     }
+#endif
---
+++ examples/random/RandomProcessor.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "LatticeProcessorModule.h"
+#include <iterator>
+
+class RandomProcessor : public LatticeProcessorModule
+{
+public:
+    RandomProcessor();
+    
+    ChannelData createChannels() override;
+    
+    /* This function is called by he host to populate the parameter vector */
+    ParameterData createParameters() override;
+
+    /*  This function is called by the host before playback/performance */
+    void prepareProcessor(int sr, std::size_t block) override;
+    
+    /*  Main processing function called continuously by the host on the audio thread.
+        paramValues is a list of parameter values passed from the host in order of their creation */
+	void process(float** buffer, int numChannels, std::size_t blockSize, const HostData hostInfo) override;
+    
+    void createDescription(std::string& description)
+    {
+        description = "";
+    }
+
+    
+    void hostParameterChanged(const char* parameterID, float newValue) override;
+    const char* getModuleName() override {    return "Random Switch";     }
+
+
+
+private:
+    float samplingRate = 44100;
+	float returnValue = 0;
+    int sampleIndex = 0;
+};
+
