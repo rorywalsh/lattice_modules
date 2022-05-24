25b7a19bc78d341fddfd68b16f756c483ba0b313
Victor Lazzarini <victor.lazzarini@mu.ie>
2022-05-24 11:11:59 +0100

    adsr added

---
+++ examples/adsr/ADSRProcessor.cpp
@@ -0,0 +1,74 @@
+
+#include "ADSRProcessor.h"
+#include <iterator>
+#include <sstream>
+#include <time.h>
+
+const double twopi = 2*M_PI;
+
+ADSRProcessor::ADSRProcessor()
+  :LatticeProcessorModule(), att(0), dec(0), sus(1), env(att,dec,sus,0.1,Aurora::def_sr), retrig(false)
+{
+}
+
+LatticeProcessorModule::ChannelData ADSRProcessor::createChannels()
+{
+  addChannel({ "gate", ChannelType::input });
+  addChannel({ "output", ChannelType::output });
+  return {getChannels(), getNumberOfChannels()};
+}
+
+
+
+LatticeProcessorModule::ParameterData ADSRProcessor::createParameters()
+{
+  addParameter({ "Gate", {0, 1, 0, 1, 1}, Parameter::Type::Momentary});
+  addParameter({ "Retrigger", {0, 1, 0, 1, 1}, Parameter::Type::Momentary});
+  addParameter({"attack", {0.f, 1.f, 0.01f, 0.001f, 1.f}});
+  addParameter({"decay", {0.f, 1.f, 0.01f, 0.001f, 1.f}});
+  addParameter({"sustain",{0.f, 1.f, 1.f, 0.001f, 1.f}});
+  addParameter({"release",{0.f, 1.f, 0.1f, 0.001f, 1.f}});;
+  return {getParameters(), getNumberOfParameters()};
+}
+
+void ADSRProcessor::prepareProcessor(int sr, std::size_t block)
+{
+  env.reset(sr);
+}  
+
+void ADSRProcessor::hostParameterChanged(const char* parameterID, float newValue) {
+   const std::string paramName = getParameterNameFromId(parameterID);
+   if(paramName == "attack") att = getParameter(paramName);
+   else if(paramName == "decay") dec = getParameter(paramName);
+   else if(paramName == "sustain") sus = getParameter(paramName);
+   else if(paramName == "release") env.release(getParameter(paramName));
+   else if(paramName == "retrigger") {
+     if(getParameter(paramName) || !retrig) {
+       env.retrigger();
+       retrig = true;
+     } else if(!getParameter(paramName)) retrig = false;
+   }    
+}
+
+void ADSRProcessor::process(float** buffer, int /*numChannels*/, std::size_t blockSize, const HostData)
+{
+  env.vsize(blockSize);
+  auto &e = env(getParameter("Gate"));
+  std::copy(e.begin(),e.end(), buffer[0]);  
+}
+
+// the class factories
+#ifdef WIN32
+extern "C" 
+{
+  __declspec(dllexport) LatticeProcessorModule* create() { return new ADSRProcessor; }
+};
+
+extern "C" 
+{
+  __declspec(dllexport) void destroy(LatticeProcessorModule* p) { delete p; }
+};
+#else
+extern "C" LatticeProcessorModule* create(){             return new ADSRProcessor;         }
+extern "C" void destroy(LatticeProcessorModule* p){      delete p;                     }
+#endif
---
+++ examples/adsr/ADSRProcessor.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "LatticeProcessorModule.h"
+#include <iterator>
+#include "simple_svg_1.0.0.hpp"
+#include "Env.h"
+
+class ADSRProcessor : public LatticeProcessorModule
+{
+public:
+    ADSRProcessor();
+    
+    ChannelData createChannels() override;
+    
+    /* This function is called by he host to populate the parameter vector */
+    ParameterData createParameters() override;
+    
+    /*  This function is called by the host before playback/performance */
+    void prepareProcessor(int sr, std::size_t block) override;
+
+    /*  Main processing function called continuously by the host on the audio thread.
+        paramValues is a list of parameter values passed from the host in order of their creation */
+    void process(float** buffer, int numChannels, std::size_t blockSize, const HostData hostInfo) override;
+
+    void hostParameterChanged(const char* parameterID, float newValue) override;
+
+    const char* getModuleName() override {    return "ADSR";     }   
+    
+    ModuleType getModuleType() override
+    {
+        return ModuleType::automator;
+    }
+    
+private:
+    float att, dec, sus;
+    Aurora::Env<float> env;
+    bool retrig;
+};
+
