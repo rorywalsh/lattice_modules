fdb9c8c1b64586ea61596ee4e177bbb19af3f670
Victor Lazzarini <victor.lazzarini@mu.ie>
2022-05-26 09:47:22 +0100

    added freq shifter

---
+++ examples/freqshift/FShiftProcessor.cpp
@@ -0,0 +1,58 @@
+
+#include "FShiftProcessor.h"
+#include <iterator>
+#include <sstream>
+
+
+FShiftProcessor::FShiftProcessor()
+  :LatticeProcessorModule(), in(def_vsize), fshift(def_sr)
+{
+
+}
+
+LatticeProcessorModule::ChannelData FShiftProcessor::createChannels()
+{
+	addChannel({ "input", ChannelType::input });
+	addChannel({ "up shift", ChannelType::output });
+	addChannel({ "down shift", ChannelType::output });
+	return {getChannels(), getNumberOfChannels()};
+}
+
+
+
+LatticeProcessorModule::ParameterData FShiftProcessor::createParameters()
+{
+  addParameter({"Shift Amount", {-5000.f, 5000.f, 0.f, 1.f, 1.f}});
+    return {getParameters(), getNumberOfParameters()};
+}
+
+void FShiftProcessor::prepareProcessor(int sr, std::size_t block) {
+  fshift.reset(sr);
+}  
+
+
+void FShiftProcessor::process(float** buffer, int /*numChannels*/, std::size_t blocksize, const HostData)
+{
+  in.resize(blocksize);
+  std::copy(buffer[0],buffer[0]+blocksize,in.begin());
+  auto &up = fshift(in, getParameter("Shift Amount"));
+  auto &down = fshift.downshift();
+  std::copy(up.begin(), up.end(),buffer[0]);
+  std::copy(down.begin(), down.end(),buffer[0]); 		      
+}
+
+// the class factories
+#ifdef WIN32
+extern "C" 
+{
+	__declspec(dllexport) LatticeProcessorModule* create() { return new FShiftProcessor; }
+};
+
+extern "C" 
+{
+	__declspec(dllexport) void destroy(LatticeProcessorModule* p) { delete p; }
+};
+#else
+extern "C" LatticeProcessorModule* create(){             return new FShiftProcessor;         }
+extern "C" void destroy(LatticeProcessorModule* p){      delete p;                     }
+#endif
---
+++ examples/freqshift/FShiftProcessor.h
@@ -0,0 +1,86 @@
+#pragma once
+#include "LatticeProcessorModule.h"
+#include <iterator>
+#include "simple_svg_1.0.0.hpp"
+#include "Quad.h"
+#include "Osc.h"
+
+using namespace Aurora;
+class FreqShifter {
+  Quad<float> quad;
+  Osc<float,phase> ph;
+  std::vector<float> cost;
+  std::vector<float> sint;  
+  std::vector<float> up;
+  std::vector<float> down;
+
+public:
+  FreqShifter(float sr, std::size_t vsize = def_vsize)
+    :   quad(sr,vsize), ph(sr,vsize), cost(def_ftlen+1), sint(def_ftlen+1),
+    up(vsize), down(vsize) {
+    for(std::size_t n = 0; n < def_ftlen+1; n++) {
+      cost[n] = std::cos(n*twopi/def_ftlen);
+      sint[n] = std::sin(n*twopi/def_ftlen);
+    }
+  };
+
+
+  void reset(float sr) {
+    ph.reset(sr);
+    quad.reset(sr);
+  }
+
+
+  const std::vector<float> &operator()(const std::vector<float> &in, float fr) {
+    std::size_t n = 0;
+    float re, im;
+    up.resize(in.size());
+    down.resize(in.size());
+    auto &phase = ph(1, fr);
+    auto &inr = quad(in);
+    auto &ini = quad.imag();
+    for(auto &s : up) {
+      re = inr[n]*lookupi(phase[n],&cost);
+      im = ini[n]*lookupi(phase[n],&sint);
+      s = re - im;
+      down[n++] = re + im;
+    }
+    return up;
+  }
+
+  const std::vector<float> &downshift() {
+    return down;
+  }
+  
+};
+
+class FShiftProcessor : public LatticeProcessorModule
+{
+public:
+    FShiftProcessor();
+    
+    ChannelData createChannels() override;
+    
+    /* This function is called by he host to populate the parameter vector */
+    ParameterData createParameters() override;
+    
+    /*  This function is called by the host before playback/performance */
+    void prepareProcessor(int sr, std::size_t block) override;
+
+    /*  Main processing function called continuously by the host on the audio thread.
+        paramValues is a list of parameter values passed from the host in order of their creation */
+    void process(float** buffer, int numChannels, std::size_t blockSize, const HostData hostInfo) override;
+
+    int getModuleType() override
+    {
+      return ModuleType::AudioProcessor::filters;
+    }
+ 
+    
+    const char* getModuleName() override {    return "Frequency Shifter";     }
+    
+private:
+    std::vector<float> in;
+    FreqShifter fshift;
+};
+
