44f722507e2b5ce079a8a7194d9b15934e10b1e1
Victor Lazzarini <victor.lazzarini@mu.ie>
2022-06-17 10:23:01 +0100

    control AM

--- examples/leslie/VibratoProcessor.cpp
+++ examples/leslie/VibratoProcessor.cpp
@@ -12,6 +12,7 @@ VibratoProcessor::VibratoProcessor()
 LatticeProcessorModule::ParameterData VibratoProcessor::createParameters()
 {
     addParameter({ "Speed", {0, 1, 0, 0.001, 1}});
+    addParameter({ "AM", {0, 1, 0.1, 0.001, 1}});
     return ParameterData(getParameters(), getNumberOfParameters());
 }
 
@@ -43,7 +44,7 @@ void VibratoProcessor::process(float** buffer, int numChannels, std::size_t bloc
     std::copy(buffer[0], buffer[0] + blockSize, in.begin());
     const float speed = 0.8 + getParameter("Speed")*6;
     
-    vibr(in,sm(speed,1,fs/blockSize),0.1);
+    vibr(in,sm(speed,1,fs/blockSize),getParameter("AM"));
     std::copy(vibr.sumL.begin(),vibr.sumL.end(),buffer[0]);
     std::copy(vibr.sumR.begin(),vibr.sumR.end(),buffer[1]);
 }
