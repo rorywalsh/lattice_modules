aafa120b81e56252657519112899521ba7924477
rorywalsh <rwalsh@Rorys-Mac-mini.fritz.box>
2022-10-21 10:28:36 +0100

    updating feedback delay module to minimise zipper noise - build number:0.1.206

--- modules/delay/DelayProcessor.cpp
+++ modules/delay/DelayProcessor.cpp
@@ -49,9 +49,9 @@ void DelayProcessor::process(float** buffer, std::size_t blockSize)
     inR.resize(blockSize);  
     std::copy(buffer[0], buffer[0] + blockSize, inL.begin());
     std::copy(buffer[1], buffer[1] + blockSize, inR.begin());
-    auto &outL = delayL(inL, timeL(getParameter("Delay Time Left"), 0.01),
+    auto &outL = delayL(inL, timeL(getParameter("Delay Time Left"), 0.0001),
 			getParameter("Feedback Left"), getParameter("Dry Mix Left"));
-    auto &outR = delayR(inR, timeR(getParameter("Delay Time Right"), 0.01),
+    auto &outR = delayR(inR, timeR(getParameter("Delay Time Right"), 0.0001),
 			getParameter("Feedback Right"), getParameter("Dry Mix Right"));
     std::copy(outL.begin(), outL.end(), buffer[0]);
     std::copy(outR.begin(), outR.end(), buffer[1]);
