e7729f6a4de314b3f4d9aa5aed180c88d510c0b4
rorywalsh <rwalsh@Rorys-Mac-mini.fritz.box>
2022-10-22 11:05:57 +0100

    adding mixer4 module - build number:0.1.207

---
+++ modules/mixer4/Mixer4.cpp
@@ -0,0 +1,94 @@
+
+#include "Mixer4.h"
+#include <iterator>
+#include <sstream>
+
+
+Mixer4::Mixer4()
+	:LatticeProcessorModule()
+{
+
+}
+
+LatticeProcessorModule::ChannelData Mixer4::createChannels()
+{
+	addChannel({ "input1", ChannelType::input });
+	addChannel({ "input2", ChannelType::input });
+    addChannel({ "input3", ChannelType::input });
+    addChannel({ "input4", ChannelType::input });
+	addChannel({ "outputL", ChannelType::output });
+    addChannel({ "outputR", ChannelType::output });
+	return {getChannels(), getNumberOfChannels()};
+}
+
+
+
+LatticeProcessorModule::ParameterData Mixer4::createParameters()
+{
+    addParameter({"Gain 1", LatticeProcessorModule::Parameter::Range(0.f, 1.f, 0.5f, 0.001f, 1.f), Parameter::Type::Slider, true});
+    addParameter({"Pan 1", LatticeProcessorModule::Parameter::Range(-1.f, 1.f, 0.0f, 0.001f, 1.f), Parameter::Type::Slider, true});
+    addParameter({"Gain 2", LatticeProcessorModule::Parameter::Range(0.f, 1.f, 0.5f, 0.001f, 1.f), Parameter::Type::Slider, true});
+    addParameter({"Pan 2", LatticeProcessorModule::Parameter::Range(-1.f, 1.f, 0.0f, 0.001f, 1.f), Parameter::Type::Slider, true});
+    addParameter({"Gain 3", LatticeProcessorModule::Parameter::Range(0.f, 1.f, 0.5f, 0.001f, 1.f), Parameter::Type::Slider, true});
+    addParameter({"Pan 3", LatticeProcessorModule::Parameter::Range(-1.f, 1.f, 0.0f, 0.001f, 1.f), Parameter::Type::Slider, true});
+    addParameter({"Gain 4", LatticeProcessorModule::Parameter::Range(0.f, 1.f, 0.5f, 0.001f, 1.f), Parameter::Type::Slider, true});
+    addParameter({"Pan 4", LatticeProcessorModule::Parameter::Range(-1.f, 1.f, 0.0f, 0.001f, 1.f), Parameter::Type::Slider, true});
+    return {getParameters(), getNumberOfParameters()};
+}
+
+void Mixer4::prepareProcessor(int sr, std::size_t block)
+{
+  fs = sr;
+}  
+
+
+void Mixer4::process(float** buffer, std::size_t blockSize)
+{
+    float g1 = gSmooth1(getParameter("Gain 1"), 0.001);
+    float g2 = gSmooth2(getParameter("Gain 2"), 0.001);
+    float g3 = gSmooth3(getParameter("Gain 3"), 0.001);
+    float g4 = gSmooth4(getParameter("Gain 4"), 0.001);
+    float p1 = pSmooth1(((getParameter("Pan 1") + 1.f) / 2.f), 0.01f);;
+    float p2 = pSmooth2(((getParameter("Pan 2") + 1.f) / 2.f), 0.01f);;
+    float p3 = pSmooth3(((getParameter("Pan 3") + 1.f) / 2.f), 0.01f);;
+    float p4 = pSmooth4(((getParameter("Pan 4") + 1.f) / 2.f), 0.01f);;
+    
+    
+    for(int i = 0; i < blockSize ; i++)
+    {
+        float in1 = buffer[0][i];
+        float in2 = buffer[1][i];
+        float in3 = buffer[2][i];
+        float in4 = buffer[3][i];
+        
+        buffer[0][i] = (1-p1)*(isInputConnected(0) ? in1 * g1 : 0);
+        buffer[1][i] = (p1)*(isInputConnected(0) ? in1 * g1 : 0);
+        
+        buffer[0][i] += (1-p2)*(isInputConnected(1) ? in2 * g2 : 0);
+        buffer[1][i] += (p2)*(isInputConnected(1) ? in2 * g2 : 0);
+        
+        buffer[0][i] += (1-p3)*(isInputConnected(2) ? in3 * g3 : 0);
+        buffer[1][i] += (p3)*(isInputConnected(2) ? in3 * g3 : 0);
+        
+        buffer[0][i] += (1-p4)*(isInputConnected(3) ? in4 * g4 : 0);
+        buffer[1][i] += (p4)*(isInputConnected(3) ? in4 * g4 : 0);
+    }
+
+}
+
+
+// the class factories
+#ifdef WIN32
+extern "C" 
+{
+	__declspec(dllexport) LatticeProcessorModule* create() { return new Mixer4; }
+};
+
+extern "C" 
+{
+	__declspec(dllexport) void destroy(LatticeProcessorModule* p) { delete p; }
+};
+#else
+extern "C" LatticeProcessorModule* create(){             return new Mixer4;         }
+extern "C" void destroy(LatticeProcessorModule* p){      delete p;                     }
+#endif
---
+++ modules/mixer4/Mixer4.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "LatticeProcessorModule.h"
+#include <iterator>
+#include "simple_svg_1.0.0.hpp"
+
+class Mixer4 : public LatticeProcessorModule
+{
+public:
+    Mixer4();
+    
+    ChannelData createChannels() override;
+    
+    /* This function is called by he host to populate the parameter vector */
+    ParameterData createParameters() override;
+    
+    /*  This function is called by the host before playback/performance */
+    void prepareProcessor(int sr, std::size_t block) override;
+
+    /*  Main processing function called continuously by the host on the audio thread.
+        paramValues is a list of parameter values passed from the host in order of their creation */
+    void process(float** buffer, std::size_t blockSize) override;
+ 
+    const char* getModuleName() override {    return "Mixer4";     }
+    
+private:
+	static int remap(float value, float rangeMin, float rangeMax, float newRangeMin, float newRangeMax)
+	{
+		return static_cast<int>(newRangeMin + (value - rangeMin) * (newRangeMax - newRangeMin) / (rangeMax - rangeMin));
+	}
+
+    ParamSmooth gSmooth1, gSmooth2, gSmooth3, gSmooth4, pSmooth1, pSmooth2, pSmooth3, pSmooth4;
+    float fs = 44100;
+
+};
+
