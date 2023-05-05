7487dd1aeb84b10928fc77073938c99c5607b8ab
Rory Walsh <rorywalsh@ear.ie>
2023-05-05 15:15:26 +0100

    updating node names to use brackets instead of pipes - build number:0.1.216

--- include/LatticeProcessorModule.h
+++ include/LatticeProcessorModule.h
@@ -589,7 +589,7 @@ public:
     */
     std::string getParameterNameFromId(std::string parameterId)
     {
-        return parameterId.substr(parameterId.find_last_of("|") + 2);
+        return parameterId.substr(parameterId.find_first_of(")") + 2);
     }
 
     /** override this method if you want to draw to the Lattice generic editor viewport
